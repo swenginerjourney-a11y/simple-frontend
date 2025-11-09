5a3f9b610a0b09547ffdf9401c2f0842438550fe
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2025-11-09 14:13:13 +0700

    refactor(login-form): remove sign up link from login form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -7,12 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from '@/components/ui/field';
+import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 
 export function LoginForm({
@@ -48,9 +43,6 @@ export function LoginForm({
               </Field>
               <Field>
                 <Button type='submit'>Login</Button>
-                <FieldDescription className='text-center'>
-                  Don&apos;t have an account? <a href='#'>Sign up</a>
-                </FieldDescription>
               </Field>
             </FieldGroup>
           </form>
