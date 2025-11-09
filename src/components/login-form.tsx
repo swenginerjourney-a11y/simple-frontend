ace682437301e66c10697d6575ede28fadc69910
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2025-11-09 13:47:23 +0700

    feat(theme): add theme provider and dark mode support
    implement dark mode theme support using next-themes provider
    remove unused login form elements for cleaner UI

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -43,20 +43,11 @@ export function LoginForm({
               <Field>
                 <div className='flex items-center'>
                   <FieldLabel htmlFor='password'>Password</FieldLabel>
-                  <a
-                    href='#'
-                    className='ml-auto inline-block text-sm underline-offset-4 hover:underline'
-                  >
-                    Forgot your password?
-                  </a>
                 </div>
                 <Input id='password' type='password' required />
               </Field>
               <Field>
                 <Button type='submit'>Login</Button>
-                <Button variant='outline' type='button'>
-                  Login with Google
-                </Button>
                 <FieldDescription className='text-center'>
                   Don&apos;t have an account? <a href='#'>Sign up</a>
                 </FieldDescription>
